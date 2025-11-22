05a8114ccd4547ea9586c1c020ceacd7d8b675b7
hashwarp <79776344+hashwarp@users.noreply.github.com>
2025-11-21 23:45:47 -0800

    export router types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import type * as Arken from '@arken/node/types';
 import * as dotenv from 'dotenv';
-import { Router as Router2 } from './router';
+import * as Schema from '@arken/node/schema';
+import { createRouter as createRouter2 } from './router';
 import type * as Types from './types';
 
 export * as Evolution from './modules/evolution';
@@ -12,5 +13,9 @@ export { Application } from './types';
 export type { Types };
 
 export { createRouter } from './router';
+
+export type Router = ReturnType<typeof createRouter2>;
+export type RouterInput = Schema.inferRouterInputs<Router>;
+export type RouterOutput = Schema.inferRouterOutputs<Router>;
 
 dotenv.config();
